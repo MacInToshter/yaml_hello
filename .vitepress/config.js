451757504871e41c673db97d9a59b773f5ff0045
MacInToshter <103314344+MacInToshter@users.noreply.github.com>
2023-07-12 12:46:31 +0200

    Update config.js

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "/yaml_hello/",
+  base: "/yaml_hello/",  
   title: "Simple VitePress Site",
   description: "Training Page for Cloud Computing Course",
   themeConfig: {
